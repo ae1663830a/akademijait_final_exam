9208d2c489c95b3222ead9bf721e45fcfaef70e5
Andrej Orševskij <a.orsevskij@gmail.com>
2018-06-27 09:32:11 +0300

    init commit

---
+++ andrejo-app/andrejo/src/main/java/lt/akademija/andrejo/AndrejoApplication.java
@@ -0,0 +1,12 @@
+package lt.akademija.andrejo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndrejoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndrejoApplication.class, args);
+    }
+}
