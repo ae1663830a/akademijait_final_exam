a9ffa5a670cf8d7c9382d8e7b2733b8ccbd2d386
Andrej Orševskij <a.orsevskij@gmail.com>
2018-06-27 13:39:57 +0300

    react project init

--- andrejo-ui/andrejo/src/index.js
+++ andrejo-ui/andrejo/src/index.js
@@ -1,8 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router} from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import objectCreationReducer from './store/reducers/objectCreationReducer'
+import fetchListReducer from "./store/reducers/fetchListReducer";
+import objectReducer from "./store/reducers/objectReducer";
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+axios.defaults.baseURL = 'http://localhost:8081/';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+    createObject: objectCreationReducer,
+    getObject: objectReducer,
+    getList: fetchListReducer,
+});
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
+const app = (
+    <Provider store={store}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>
+);
+
+ReactDOM.render(
+    app,
+    document.getElementById("root")
+);
+
 registerServiceWorker();
